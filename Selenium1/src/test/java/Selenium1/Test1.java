52cb880ba3e0f4f784005af7034ed94c7bd6255e
Training_B7C.06.13 <Training_B7C.06.13@BDC7-D-DLVHH92.dir.svc.accenture.com>
2018-07-03 12:25:00 +0530

    commit

---
+++ Selenium1/src/test/java/Selenium1/Test1.java
@@ -0,0 +1,16 @@
+package Selenium1;
+
+import org.testng.annotations.Test;
+public class Test1 {
+	 @Test
+     public void testDemoWebShop()
+     {
+		 System.out.println("in web shop");
+		 
+     }
+     @Test
+     public void testPayment()
+     {
+	  System.out.println("in test payment");
+     }
+}
---
+++ Selenium1/src/test/java/Selenium1/Test2.java
@@ -0,0 +1,26 @@
+package Selenium1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test2 {
+@Test
+public void testGitHubLogin()
+{
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\training_b7c.06.13\\Downloads\\chromedriver.exe");
+	WebDriver driver=new ChromeDriver();
+	driver.manage().window().maximize();
+	driver.get("https://github.com/login");;
+	driver.findElement(By.id("login_field")).sendKeys("joys12345");
+	driver.findElement(By.id("password")).sendKeys("Oppr1234()");
+	driver.findElement(By.name("commit")).click();
+	
+	String title=driver.getTitle();
+	Assert.assertEquals(title,"GitHub");
+	System.out.println("Continue with test code");
+	driver.get("https://guides.github.com/activities/hello-world/");;
+}
+}
